daf8fa697f0bad99de9c3afa797780c1ac537dbc
Marcelo Vieira <vieiramarcelo.vds@gmail.com>
2025-05-25 12:13:31 -0300

    finalizada a tela que vai receber a lista de funcionarios cadastrados

--- src/br/dev/marcelo/tarefas/Main.java
+++ src/br/dev/marcelo/tarefas/Main.java
@@ -10,64 +10,50 @@ import java.util.ArrayList;
 import java.util.List;
 
 import br.dev.marcelo.tarefas.dao.FuncionarioDAO;
+import br.dev.marcelo.tarefas.gui.TelaCadastroFuncionario;
 import br.dev.marcelo.tarefas.model.Funcionario;
 import br.dev.marcelo.tarefas.model.Tarefa;
 
 public class Main {
 	
+	
+	
 	//determinando caminho do arquivo que será lido
 	private static String path = "C:\\Users\\25132416\\tarefa\\tarefas.txt";
 	
 	public static void main(String[] args) {
-		
-		List<String> frutas = new ArrayList<>();
-		List<Funcionario> funcionarios = new ArrayList<>();
-		List<Integer> inteiros = new ArrayList<>();
-		
-		frutas.add("abacaxi");
-		frutas.add("banana");
-		frutas.add("maçã");
-		frutas.add("uva");
-		frutas.add("morango");
-		
-		inteiros.add(4);
-		inteiros.add(6);
-		inteiros.add(9);
-		
-		Funcionario funcionario = new Funcionario();
-		funcionario.setCodigo(4);
-		funcionario.setNome("Tiago");
-		funcionario.setMatricula("2516090");
-		funcionario.setEmail("tiago@gmail.com");
-		
-		Funcionario funcionario2 = new Funcionario();
-		funcionario2.setCodigo(5);
-		funcionario2.setNome("Marcos");
-		funcionario2.setMatricula("2518090");
-		funcionario2.setEmail("marcos@gmail.com");
-		
-		Funcionario funcionario3 = new Funcionario();
-		funcionario3.setCodigo(6);
-		funcionario3.setNome("Hugo");
-		funcionario3.setMatricula("2518090");
-		funcionario3.setEmail("hugo@gmail.com");
-		
-		funcionarios.addAll(List.of(funcionario, funcionario2, funcionario3));
-		
-		System.out.println(frutas);
-		System.out.println(inteiros);
-		System.out.println(funcionarios);
-		
-		for (Funcionario f : funcionarios) {
-			System.out.println(f.getNome() + " - " + f.getEmail());
-		}
-		
-//		
-//		FuncionarioDAO dao = new FuncionarioDAO(funcionario);
-//		dao.gravar();
-		
-	}
 	
+	TelaCadastroFuncionario tela = new TelaCadastroFuncionario();
+	tela.criarTela();
+//		
+//		List<Funcionario> funcionarios = new ArrayList<>();
+//		
+//		Funcionario funcionario = new Funcionario();
+//		funcionario.setCodigo(4);
+//		funcionario.setNome("Tiago");
+//		funcionario.setMatricula("2516090");
+//		funcionario.setEmail("tiago@gmail.com");
+//		
+//		Funcionario funcionario2 = new Funcionario();
+//		funcionario2.setCodigo(5);
+//		funcionario2.setNome("Marcos");
+//		funcionario2.setMatricula("2518090");
+//		funcionario2.setEmail("marcos@gmail.com");
+//		
+//		funcionarios.addAll(List.of(funcionario, funcionario2));
+//		
+//		for (Funcionario f : funcionarios) {
+//			System.out.println(f.getMatricula() + "   -   " + f.getNome() + "   -   " + f.getEmail());
+//		}
+//		
+//		System.out.println(funcionarios.toString()); 
+		
+////		
+////		FuncionarioDAO dao = new FuncionarioDAO(funcionario);
+////		dao.gravar();
+//		
+	}
+//	
 	private static void gravarArquivo() {
 		
 		FileWriter arquivo = null;
@@ -116,7 +102,7 @@ public class Main {
 		}
 		
 		
-		
+//		
 	}
 
 }
